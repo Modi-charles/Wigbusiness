1abe2c6dc642e478183ad3dd0be978a55465e11d
Modi  Charles <modicharles030@gmail.com>
2026-09-26 17:12:23 +0300

    updatedversion1.7

--- wigbiz/wigbiz/settings.py
+++ wigbiz/wigbiz/settings.py
@@ -36,7 +36,6 @@ DEBUG = os.environ.get("DJANGO_DEBUG", "true").lower() in {
 
 configured_hosts = os.environ.get("DJANGO_ALLOWED_HOSTS", "*")
 ALLOWED_HOSTS = [
-    '.vercel.app',
     host.strip()
     for host in configured_hosts.split(",")
     if host.strip()
